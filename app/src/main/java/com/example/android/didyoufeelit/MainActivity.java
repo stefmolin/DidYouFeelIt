47aaabd0d91ee4306b277a1abdae465d07cc0e14
Stefanie Molin <stefaniemolin@gmail.com>
2018-02-19 08:37:48 -0500

    Handle for situations when inputs aren't as expected

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -62,17 +62,41 @@ public class MainActivity extends AppCompatActivity {
      * Handle network requests in the background and update the UI when done
      */
     private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event> {
+        /**
+         * This method is invoked (or called) on a background thread, so we can perform
+         * long-running operations like making a network request.
+         *
+         * It is NOT okay to update the UI from a background thread, so we just return an
+         * {@link Event} object as the result.
+         */
         @Override
         protected Event doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
             // Perform the HTTP request for earthquake data and process the response.
             Event earthquake = Utils.fetchEarthquakeData(urls[0]);
             return earthquake;
         }
 
+        /**
+         * This method is invoked on the main UI thread after the background work has been
+         * completed.
+         *
+         * It IS okay to modify the UI within this method. We take the {@link Event} object
+         * (which was returned from the doInBackground() method) and update the views on the screen.
+         */
         @Override
-        protected void onPostExecute(Event event) {
+        protected void onPostExecute(Event result) {
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
+
             // Update the information displayed to the user.
-            updateUi(event);
+            updateUi(result);
         }
     }
 }
